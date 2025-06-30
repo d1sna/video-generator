33f4b56d270eb9ff2fdfe7131772596959729594
Aleksandr Dis <a.dis@awatera.com>
2025-06-30 19:45:55 +0300

    fixed port

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	plugins: [tailwindcss()],
-	server: { allowedHosts: ["3e7a-94-228-164-237.ngrok-free.app"] },
+	server: {
+		allowedHosts: ["3e7a-94-228-164-237.ngrok-free.app"],
+		port: Number(process.env.PORT) || 4000,
+	},
 });
